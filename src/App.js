e02aa960049e813eae160b26c02ae1329ee3894c
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2021-12-21 00:17:18 -0800

    feat: useState practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, titleEdit] = useState(['Men Jacket Review', 'Men Pants Review']);
+  let [title2, titleEdit2] = useState('Women Dress Review');
 
   let posts = 'Vancouver based full-stact developer'
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{color:'lavender', fontSize:'30pt'}}>Blog by React</div>
+        {/* <div style={{color:'lavender', fontSize:'30pt'}}>Blog by React</div> */}
+        <div>Blog by React</div>
       </div>
-      <img src= {logo} alt="logo" />
-      <h4> {posts} </h4>
+      
+      <div className="list">
+        <h3> {title[0]} </h3>
+        <p>Published by December 2021</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> {title2} </h3>
+        <p>Published by December 2021</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> {posts} </h3>
+        <p>Published by December 2021</p>
+        <hr/>
+      </div>
+      
     </div>
   );
 }
